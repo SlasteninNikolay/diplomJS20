f6ca5ae72d81525475a5863152b51b6f4c7419d7
Slastenin Nikolay <slastenin13@gmail.com>
2021-09-14 02:05:24 +0300

    Написал скрипт для модального окна заказа звонка

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,5 +1,22 @@
 const popUp = () => {
-    console.log("popUp");
+    const headerModal = document.querySelector(".header-modal");
+    const overlay = document.querySelector(".overlay");
+
+    const handlerPopUp = (e) => {
+        const target = e.target;
+        if (target.closest(".btn-callback")) {
+            headerModal.style.display = "block";
+            overlay.style.display = "block";
+            document.body.style.overflow = "hidden";
+        }
+        if (target.matches(".header-modal__close")) {
+            headerModal.style.display = "none";
+            overlay.style.display = "none";
+            document.body.style.overflow = "";
+        }
+    };
+
+    document.body.addEventListener("click", handlerPopUp);
 };
 
 export default popUp;
