6d2610e2c648223aa0cb37c45bd87ffdf085ce6c
Slastenin Nikolay <slastenin13@gmail.com>
2021-09-15 22:53:14 +0300

    Добавил открытие и закрытие модального окна services-modal

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -1,6 +1,7 @@
 const popUp = () => {
     const headerModal = document.querySelector(".header-modal");
     const overlay = document.querySelector(".overlay");
+    const servicesModal = document.querySelector(".services-modal");
 
     const handlerPopUp = (e) => {
         const target = e.target;
@@ -12,6 +13,17 @@ const popUp = () => {
         }
         if (target.matches(".header-modal__close")) {
             headerModal.style.display = "none";
+            overlay.style.display = "none";
+            document.body.style.overflow = "";
+        }
+        if (target.closest(".service-button")) {
+            console.log(target);
+            servicesModal.style.display = "block";
+            overlay.style.display = "block";
+            document.body.style.overflow = "hidden";
+        }
+        if (target.matches(".services-modal__close")) {
+            servicesModal.style.display = "none";
             overlay.style.display = "none";
             document.body.style.overflow = "";
         }
