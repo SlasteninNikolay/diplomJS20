9df935b8e1b19bf6dd1f143f4208a2b461181f27
Slastenin Nikolay <slastenin13@gmail.com>
2021-09-18 01:10:07 +0300

    Написал скрипт модального окна для сертификатов

--- src/modules/showSertificate.js
+++ src/modules/showSertificate.js
@@ -1,5 +1,28 @@
 const showSertificate = () => {
-    console.log("showSertificate");
+    const documents = document.querySelector("#documents");
+    const overlay = document.querySelector(".overlay");
+    const documentModal = document.querySelector(".document-modal");
+    const img = document.querySelector(".document-modal img");
+    const documentClose = document.querySelector(".document-close");
+
+    documents.addEventListener("click", (event) => {
+        const target = event.target;
+        if (target.matches(".document-overlay")) {
+            event.preventDefault();
+            overlay.style.display = "block";
+            const link = target.closest("a").getAttribute("href");
+            img.setAttribute("src", link);
+            documentModal.classList.toggle("document-modal__active");
+        }
+    });
+    documentClose.addEventListener("click", () => {
+        overlay.style.display = "none";
+        documentModal.classList.toggle("document-modal__active");
+    });
+    overlay.addEventListener("click", () => {
+        overlay.style.display = "none";
+        documentModal.classList.toggle("document-modal__active");
+    });
 };
 
 export default showSertificate;
