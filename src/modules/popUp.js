e6b31a6bae2560532e7c27cac5a250b1094ff10d
Slastenin Nikolay <slastenin13@gmail.com>
2021-09-17 21:53:54 +0300

    Исправил preventDefault

--- src/modules/popUp.js
+++ src/modules/popUp.js
@@ -2,33 +2,34 @@ const popUp = () => {
     const headerModal = document.querySelector(".header-modal");
     const overlay = document.querySelector(".overlay");
     const servicesModal = document.querySelector(".services-modal");
-
     const handlerPopUp = (e) => {
         const target = e.target;
-        e.preventDefault();
         if (target.closest(".btn-callback")) {
+            e.preventDefault();
             headerModal.style.display = "block";
             overlay.style.display = "block";
             document.body.style.overflow = "hidden";
         }
         if (target.matches(".header-modal__close")) {
+            e.preventDefault();
             headerModal.style.display = "none";
             overlay.style.display = "none";
             document.body.style.overflow = "";
         }
         if (target.closest(".service-button")) {
+            e.preventDefault();
             console.log(target);
             servicesModal.style.display = "block";
             overlay.style.display = "block";
             document.body.style.overflow = "hidden";
         }
         if (target.matches(".services-modal__close")) {
+            e.preventDefault();
             servicesModal.style.display = "none";
             overlay.style.display = "none";
             document.body.style.overflow = "";
         }
     };
-
     document.body.addEventListener("click", handlerPopUp);
 };
 
