ee567901171f93002b8e722a38f76fca68190dd9
Slastenin Nikolay <slastenin13@gmail.com>
2021-09-18 22:57:00 +0300

    Написал скрипт для калькулятора

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,6 +1,35 @@
 const calculator = () => {
-    const test = "test";
-    console.log(test);
+    const calcSection = document.getElementById("calc");
+    const calcTotal = document.getElementById("calc-total");
+    console.log("calcTotal: ", calcTotal);
+
+    if (calcSection) {
+        let calcTypeValue;
+        let calcMaterialValue;
+        let calcInput;
+
+        const countTotal = () => {
+            if (calcTypeValue && calcMaterialValue && calcInput) {
+                let result = (calcTypeValue * calcMaterialValue * calcInput).toFixed(2);
+                localStorage.setItem("Итого", result);
+                calcTotal.value = result;
+            }
+        };
+
+        calcSection.addEventListener("input", (event) => {
+            const target = event.target;
+            if (target.matches("#calc-type")) {
+                calcTypeValue = +target.value;
+            }
+            if (target.matches("#calc-type-material")) {
+                calcMaterialValue = +target.value;
+            }
+            if (target.matches("#calc-input")) {
+                calcInput = +target.value;
+            }
+            countTotal();
+        });
+    }
 };
 
 export default calculator;
